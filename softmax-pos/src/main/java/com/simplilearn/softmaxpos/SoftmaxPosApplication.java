bf745d08ca3b6c4f001eb3740dba724bcb4f9c15
student <ronald.carson@ngc.com>
2021-03-12 22:07:59 +0000

    Add project softmax

---
+++ softmax-pos/src/main/java/com/simplilearn/softmaxpos/SoftmaxPosApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.softmaxpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftmaxPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftmaxPosApplication.class, args);
+	}
+
+}
---
+++ softmax-pos/src/test/java/com/simplilearn/softmaxpos/SoftmaxPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.softmaxpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftmaxPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
